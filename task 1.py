4a3269c67234a98ea17b269f98874190ea6395ef
Dakshata123 <dakshatakarmore87@gmail.com>
2024-09-21 17:01:02 +0530

    Update task 1.py

--- task 1.py
+++ task 1.py
@@ -1,7 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 21 16:33:16 2024
+def insert_at_bottom(stack, item):
+    if not stack:
+        stack.append(item)
+    else:
+        temp = stack.pop()
+        insert_at_bottom(stack, item)
+        stack.append(temp)
 
-@author: a
-"""
+def reverse_stack(stack):
+    if stack:
+        temp = stack.pop()
+        reverse_stack(stack)
+        insert_at_bottom(stack, temp)
+
+# Example usage:
+stack = [1, 2, 3, 4, 5]
+reverse_stack(stack)
+print(stack)  # Output should be [5, 4, 3, 2, 1]
 
